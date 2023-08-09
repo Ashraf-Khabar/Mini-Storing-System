321f10c7a202be1ef8bff7037c34d7b013c83a30
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-08-09 09:04:46 +0100

    Created a data space

--- main.c
+++ main.c
@@ -7,30 +7,39 @@
 
 int main()
 {
-    int check = 0;
+    int check = 0, menu = 0;
     char *username = (char *)malloc(MAX_USERNAME_LENGTH * sizeof(char));
     char *password = (char *)malloc(MAX_PASSWORD_LENGTH * sizeof(char));
 
     printAsciiArt();
-    
-    while (!check)
+
+    while (check != 0 || menu != 4)
     {
-        if (username == NULL || password == NULL)
+        printf("keys : 1 => create acount / 2 => connect / 3 => Reload / => Quit\n");
+        printf("Options : 1 - 2 - 3 - 4 : ");
+        scanf("%d", &menu);
+
+        if (menu == 1)
         {
-            printf("Memory allocation failed.\n");
-            return 1;
-        }
-        printf("Enter the username: ");
-        gets(username);
-
-        printf("Enter the password: ");
-        gets(password);
-
-        printf("\n");
-
-        if (createAcount(username, password))
-        {
-            check = 1;
+            if (username == NULL || password == NULL)
+            {
+                printf("Memory allocation failed.\n");
+                return 1;
+            }
+            printf("Enter the username: ");
+            gets(username);
+            printf("Enter the password: ");
+            gets(password);
+            printf("\n");
+            if (createAcount(username, password))
+            {
+                check = 1;
+            }
+            else
+            {
+                check = 0;
+            }
+            break;
         }
         else
         {
